9722ed7cb6ca742868b3d83e16a2dae4596708fb
Sebastian Bolanos <sebasheston@protonmail.com>
2020-02-29 18:03:26 +0000

    Add query form branch

--- packages/react-app/src/App.js
+++ packages/react-app/src/App.js
@@ -1,19 +1,46 @@
 import React, { useState } from "react";
 import { addresses, abis } from "@project/contracts";
 import { ethers } from "ethers";
-import { Card, CardText, CardBody } from "reactstrap";
+//import { Card, CardText, CardBody } from "reactstrap";
 //import { bigNumberify } from "ethers/utils";
 import "./App.css";
 
-async function queryBeaconNode(setCount, path) {
+async function queryBeaconNode(path) {
   const url = "http://163.172.177.34:8080" + path;
   var response = await (await fetch(url)).json();
-  setCount(response);
+  console.log(response);
 }
 
 function handleDepositEvent(pubkey, withdrawl_creds, amount, signature, index) {
   console.log("Pubkey", pubkey);
 }
+
+const { inputs, handleInputChange, handleSubmit } = usePathForm();
+const [result, setResult] = useState("no query results");
+const [newDeposits, setNewDeposits] = useState("waiting for new deposits...");
+
+const usePathForm = callback => {
+  const [inputs, setInputs] = useState({});
+  const handleSubmit = event => {
+    if (event) {
+      event.preventDefault();
+    }
+    queryBeaconNode(inputs.newQueryPath);
+  };
+  const handleInputChange = event => {
+    event.persist();
+    setInputs(inputs => ({
+      ...inputs,
+      [event.target.name]: event.target.value
+    }));
+  };
+
+  return {
+    handleSubmit,
+    handleInputChange,
+    inputs
+  };
+};
 
 function App() {
   const goerliProvider = ethers.getDefaultProvider("goerli");
@@ -23,20 +50,25 @@ function App() {
     goerliProvider
   );
   valRegistration.on("DepositEvent", handleDepositEvent);
-  const [result, setResult] = useState("hi");
+
   return (
     <div className="App">
-      <button
-        onClick={() => queryBeaconNode(setResult, "/beacon/validators")}
-        style={{ background: "red" }}
-      >
-        queryBeaconNode
-      </button>
-      <Card>
-        <CardBody>
-          <CardText>{result}</CardText>
-        </CardBody>
-      </Card>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Path</label>
+          <input
+            type="text"
+            name="newQueryPath"
+            onChange={handleInputChange}
+            value={inputs.newQueryPath}
+            required
+          />
+        </div>
+      </form>
+
+      <h3>Query Result</h3>
+      <p>{result}</p>
+      <h3>New Deposit</h3>
     </div>
   );
 }
