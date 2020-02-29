a20ffb80cb4c5c822b4c2a0d91b90606138df51a
Sebastian Bolanos <sebasheston@protonmail.com>
2020-02-29 13:10:21 +0000

    Change var names

--- packages/contracts/src/index.js
+++ packages/contracts/src/index.js
@@ -1,11 +1,11 @@
 import erc20Abi from "./abis/erc20";
 import ownableAbi from "./abis/ownable";
-import validatorRegistraionAbi from "./abis/validator_registration";
+import validatorAbi from "./abis/validator_registration";
 
 export const abis = {
   erc20: erc20Abi,
   ownable: ownableAbi,
-  validatorRegistraion: validatorRegistraionAbi
+  validatorRegistration: validatorAbi
 };
 
 export { default as addresses } from "./addresses";
