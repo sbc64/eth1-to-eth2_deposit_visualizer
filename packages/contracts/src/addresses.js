a2d57c22c3d445fc9efc353885c6348bdfa33f1e
Sebastian Bolanos <sebasheston@protonmail.com>
2020-02-29 16:37:48 +0000

    Add beacon chain usage and URL

--- packages/contracts/src/addresses.js
+++ packages/contracts/src/addresses.js
@@ -2,7 +2,8 @@
 // Goerli, Kovan, Rinkeby and Ropsten. Replace it with your smart contracts.
 const addresses = {
   ceaErc20: "0xc1C0472c0C80bCcDC7F5D01A376Bd97a734B8815",
-  despositContractPrysm: "0x4689a3C63CE249355C8a573B5974db21D2d1b8Ef"
+  despositContractPrysm: "0x4689a3C63CE249355C8a573B5974db21D2d1b8Ef",
+  depositContractLighthouse: "0xa4d8bee071d57ccf47d2bf9f2cc8dd9151e21ec5"
 };
 
 export default addresses;
--- packages/react-app/src/App.js
+++ packages/react-app/src/App.js
@@ -1,97 +1,33 @@
 import React from "react";
-import logo from "./ethereumLogo.png";
 import { addresses, abis } from "@project/contracts";
-import { gql } from "apollo-boost";
 import { ethers } from "ethers";
-import { bigNumberify } from "ethers/utils";
-import { useQuery } from "@apollo/react-hooks";
+//import { bigNumberify } from "ethers/utils";
 import "./App.css";
 
-const GET_TRANSFERS = gql`
-  {
-    transfers(first: 10) {
-      id
-      from
-      to
-      value
-    }
-  }
-`;
+async function queryBeaconNode() {
+  const url = "http://163.172.177.34:8080";
+  const path = "/beacon/heads";
+  var response = await (await fetch(url + path)).json();
+  console.log(response);
+}
 
-async function readOnchainBalance() {
-  const goerliProvider = ethers.getDefaultProvider("goerli");
-  const defaultProvider = ethers.getDefaultProvider();
-  const ceaErc20 = new ethers.Contract(
-    addresses.ceaErc20,
-    abis.erc20,
-    defaultProvider
-  );
-  console.log(ceaErc20);
-  const tokenBalance = await ceaErc20.balanceOf(
-    "0x3f8CB69d9c0ED01923F11c829BaE4D9a4CB6c82C"
-  );
-  //console.log({ tokenBalance: tokenBalance.toString() });
-
-  const valRegistration = new ethers.Contract(
-    addresses.despositContractPrysm,
-    abis.validatorRegistration,
-    goerliProvider
-  );
-  var data = bigNumberify(await valRegistration.get_deposit_count());
-  console.log(data.toString());
-  valRegistration.on("DepositEvent", input => {
-    console.log("HI event", input);
-  });
+function handleDepositEvent(pubkey, withdrawl_creds, amount, signature, index) {
+  console.log("Pubkey", pubkey);
 }
 
 function App() {
-  const { loading, error, data } = useQuery(GET_TRANSFERS);
-
-  React.useEffect(() => {
-    if (!loading && !error && data && data.transfers) {
-      readOnchainBalance();
-    }
-  }, [loading, error, data]);
-
+  const goerliProvider = ethers.getDefaultProvider("goerli");
+  const valRegistration = new ethers.Contract(
+    addresses.depositContractLighthouse,
+    abis.validatorRegistration,
+    goerliProvider
+  );
+  valRegistration.on("DepositEvent", handleDepositEvent);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="react-logo" />
-        <p>
-          Edit <code>packages/react-app/src/App.js</code> and save to reload.
-        </p>
-        <button
-          onClick={() => readOnchainBalance()}
-          style={{ display: "none" }}
-        >
-          Read On-Chain Balance
-        </button>
-        <a
-          className="App-link"
-          href="https://ethereum.org/developers/#getting-started"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Ethereum
-        </a>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-          style={{ marginTop: "0px" }}
-        >
-          Learn React
-        </a>
-        <a
-          className="App-link"
-          href="https://thegraph.com/docs/quick-start"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn The Graph
-        </a>
-      </header>
+      <button onClick={() => queryBeaconNode()} style={{ background: "red" }}>
+        queryBeaconNode
+      </button>
     </div>
   );
 }
