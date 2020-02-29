389860caaa1816932ea19e8a7681a1f57f16eb5c
Paul Miller <paul@citywebconsultants.co.uk>
2020-02-29 19:22:15 +0000

    kitchensink

--- packages/react-app/src/App.js
+++ packages/react-app/src/App.js
@@ -1,29 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 import { addresses, abis } from "@project/contracts";
 import { ethers } from "ethers";
 //import { bigNumberify } from "ethers/utils";
 import "./App.css";
 import ValidatorInfo from './ValidatorInfo'
 
-
 async function queryBeaconNode() {
   const url = "http://163.172.177.34:8080";
-  const path = "/beacon/heads";
-  var response = await (await fetch(url + path)).json();
-  console.log(response);
+  const path = "/beacon/head";
+  const response = await (await fetch(url + path)).json();
+
+  // then make another request?
+  return response;
 }
 
 
 
 function handleDepositEvent(pubkey, withdrawl_creds, amount, signature, index) {
-
-
   console.log("Pubkey", pubkey);
 }
 
 function App() {
 
- // const [validatorId, setValidatorId] = React.useState('foo');
+ // const [stateRoot, setStateRoot] = useState('')
+
+  const [validatorId, setValidatorId] = useState('foo');
+  const [stateRoot, setStateRoot] = useState('');
 
   const goerliProvider = ethers.getDefaultProvider("goerli");
   const valRegistration = new ethers.Contract(
@@ -39,10 +41,11 @@ function App() {
         queryBeaconNode
       </button>
 
-    <div>
       {/*{validatorId} is ready to validate*/}
-      <ValidatorInfo pubKey=""></ValidatorInfo>
-    </div>
+      <ValidatorInfo
+        stateRoot='0xea81f5735fc0d76aea8e2c0dd9a6da7b7b1aa69fe26f152bce1938aa81dbb0f8'
+        pubKey='0x949d64d2942c9c084d8614c7037cbe54e9583137bfdd025b47ffc186714e6196dcf597cbae4a5a5a312f7a58d208b854'>
+      </ValidatorInfo>
     </div>
 
   );
--- packages/react-app/src/ValidatorInfo.js
+++ packages/react-app/src/ValidatorInfo.js
@@ -3,27 +3,105 @@ import React, {useState} from "react";
 // import { ethers } from "ethers";
 // //import { bigNumberify } from "ethers/utils";
 // import "./App.css";
+// list of useful api endpoints
+// these differ by service
+
+// lighthouse
+// /beacon/validators	Query for one or more validators.
+// /beacon/validators/active	Get all active validators.
+// /beacon/validators/all
 
 
-const fetchValidatorByPubKey = async () => {
- const response = await fetch('http://163.172.177.34:8080/beacon/validators/all');
- console.log(await response.json())
+// const server = 'http://163.172.177.34:8080/'
+// const path  =  'http://163.172.177.34:8080'
+
+const fetchValidatorByPubKey = async (pubKey, stateRoot) => {
+  let response;
+
+//   if (pubKey === '') {
+//        response = await fetch('http://163.172.177.34:8080/beacon/validators/all');
+//      }
+//      else {
+       const url = 'http://163.172.177.34:8080/beacon/validators'
+       const data = {
+           state_root: stateRoot,
+           pubkeys: [pubKey]
+       }
+           console.log(await postData(url, data))
+  //   }
+
+ //const response = await fetch('http://163.172.177.34:8080/beacon/validators/all');
+// console.log('SIGNLE REQUEST RESULT', await response.json());
+}
+
+async function postData(url = '', data = {}) {
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'no-cors', // no-cors, *cors, same-origin
+//     cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+//     credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+      'Content-Type': 'application/json'
+    },
+//     redirect: 'follow', // manual, *follow, error
+//     referrerPolicy: 'no-referrer', // no-referrer, *client
+     body: JSON.stringify(data) // body data type must match "Content-Type" header
+  });
+
+  return await response.json(); // parses JSON response into native JavaScript objects
 }
 
 
-export default function ValidatorInfo() {
 
-const [validatorId, setValidatorId] = useState('foo');
 
-fetchValidatorByPubKey();
+export default function ValidatorInfo(props) {
 
-return (
-    <div>Here's some info</div>
-)
+//const [validatorId, setValidatorId] = useState('foo');
+// setState when we grab stuff
+// can we have an object?
 
+//fetchValidatorByPubKey(props.pubKey, props.stateRoot);
+
+    return (
+    // mebs should put in some kind of table
+    // should wee chain this and see what other information we can gather...
+    // should be checking for an indiviauk
+        <div>Here's some info</div>
+    )
 }
 
 
+/*
+
+
+async function postData(url = '', data = {}) {
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+      'Content-Type': 'application/json'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: 'follow', // manual, *follow, error
+    referrerPolicy: 'no-referrer', // no-referrer, *client
+    body: JSON.stringify(data) // body data type must match "Content-Type" header
+  });
+  return await response.json(); // parses JSON response into native JavaScript objects
+}
+
+postData('https://example.com/answer', { answer: 42 })
+  .then((data) => {
+    console.log(data); // JSON data parsed by `response.json()` call
+  });
+
+
+
+
+*/
 
 
 
