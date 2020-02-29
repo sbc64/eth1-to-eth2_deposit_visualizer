1e349c2c65ae7da087e64709624d08d681ee3109
Sebastian Bolanos <sebasheston@protonmail.com>
2020-02-29 18:29:34 +0000

    Seperated query functions

--- packages/react-app/src/App.js
+++ packages/react-app/src/App.js
@@ -1,14 +1,18 @@
 import React, { useState } from "react";
 import { addresses, abis } from "@project/contracts";
 import { ethers } from "ethers";
-import { Card, CardText, CardBody } from "reactstrap";
 //import { bigNumberify } from "ethers/utils";
 import "./App.css";
 
-async function queryBeaconNode(setResult, path) {
+async function queryBeaconNode(path) {
   const url = "http://163.172.177.34:8080" + path;
-  var response = await (await fetch(url)).json();
-  setResult(JSON.stringify(response));
+  return await (await fetch(url)).json();
+}
+
+async function queryAllValidators(setFunction) {
+  const result = await queryBeaconNode("/beacon/validators/all");
+  console.log("RESULT: ", result);
+  setFunction(JSON.stringify(result));
 }
 
 function handleDepositEvent(pubkey, withdrawl_creds, amount, signature, index) {
@@ -16,6 +20,8 @@ function handleDepositEvent(pubkey, withdrawl_creds, amount, signature, index) {
 }
 
 function App() {
+  const [resultAllValidators, setAllvalidators] = useState("No queries yet...");
+
   const goerliProvider = ethers.getDefaultProvider("goerli");
   const valRegistration = new ethers.Contract(
     addresses.depositContractLighthouse,
@@ -23,17 +29,30 @@ function App() {
     goerliProvider
   );
   valRegistration.on("DepositEvent", handleDepositEvent);
-  const [result, setResult] = useState("No queries yet...");
   return (
     <div className="App">
-      <button
-        onClick={() => queryBeaconNode(setResult, "/beacon/validators/all")}
-        style={{ background: "red" }}
-      >
-        queryBeaconNode
-      </button>
-      <h4>Query results</h4>
-      <p>{result}</p>
+      <div>
+        <button
+          onClick={() => queryAllValidators(setAllvalidators)}
+          style={{ background: "red" }}
+        >
+          queryAllValidators
+        </button>
+        <h4>Query results</h4>
+        <p style={{ height: "300px", overflowY: "scroll" }}>
+          {resultAllValidators}
+        </p>
+      </div>
+      <div>
+        <button
+          onClick={() => queryAllValidators(setAllvalidators)}
+          style={{ background: "red" }}
+        >
+          queryAllValidators
+        </button>
+        <h4>Query results</h4>
+        <p>{}</p>
+      </div>
     </div>
   );
 }
