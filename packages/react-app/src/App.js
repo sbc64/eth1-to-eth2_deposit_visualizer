830b407d36bbeacd156c1f78de9be703de85c5ca
Sebastian Bolanos <sebasheston@protonmail.com>
2020-02-29 23:28:55 +0000

    Save before changes

--- packages/react-app/src/App.js
+++ packages/react-app/src/App.js
@@ -2,22 +2,52 @@ import React, { useState } from "react";
 import { addresses, abis } from "@project/contracts";
 import { ethers } from "ethers";
 //import { bigNumberify } from "ethers/utils";
+import { Collapse, Button, CardBody, Card } from "reactstrap";
 import "./App.css";
 
-async function queryBeaconNode(path) {
+async function queryBeaconNode(path, body) {
+  //console.log("PATH: ", path);
+  //console.log("BODY: ", body);
   const url = "http://163.172.177.34:8080" + path;
-  return await (await fetch(url)).json();
+  const response = await fetch(url, body);
+  var R = await response.json();
+  return R;
 }
 
 async function queryAllValidators(setFunction) {
   const result = await queryBeaconNode("/beacon/validators/all");
-  console.log("RESULT: ", result);
   setFunction(JSON.stringify(result));
+}
+
+async function fetchCanonicalHead() {
+  var R = await queryBeaconNode("/beacon/head");
+  return R;
+}
+
+async function countBackwards() {
+  let latestSlot = await queryBeaconNode("/beacon/head");
+  //let lowestSlot = 100000;
+  let lowestSlot = 0;
+
+  let slotsWithDeposits = [];
+  for (var i = latestSlot.slot; i >= lowestSlot; i--) {
+    let path = "/beacon/block?slot=" + i;
+    let result = await queryBeaconNode(path);
+    let deposits = result.beacon_block.message.body.deposits;
+    if (deposits.length !== 0) {
+      slotsWithDeposits.push(result.beacon_block.message.slot);
+    }
+  }
+}
+
+function displayDespoit() {
+  return <div></div>;
 }
 
 function App() {
   const [resultAllValidators, setAllvalidators] = useState("No queries yet...");
   const [depositEvent, setDepositEvent] = useState("No deposits yet...");
+  const [validator, setValidator] = useState("No deposits yet...");
 
   const goerliProvider = ethers.getDefaultProvider("goerli");
   const valRegistration = new ethers.Contract(
@@ -55,10 +85,6 @@ function App() {
         <p style={{ height: "300px", overflowY: "scroll" }}>
           {resultAllValidators}
         </p>
-      </div>
-      <div>
-        <h4>Deposit Events</h4>
-        <p>{depositEvent}</p>
       </div>
     </div>
   );
---
+++ packages/react-app/src/ValidatorInfo.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+
+// lighthouse
+// /beacon/validators	Query for one or more validators.
+// /beacon/validators/active	Get all active validators.
+// /beacon/validators/all
+
+export default function ValidatorInfo(props) {
+  const [results, setResults] = useState("no results");
+
+  // const [validatorInfo, setValidatorInfo] = useState({})
+  const [activationEligibilityEpoch, setActivationEligibilityEpoch] = useState(
+    "unknown"
+  );
+  const [activationEpoch, setActivationEpoch] = useState("unknown");
+
+  const fetchValidatorByPubKey = async (pubKey, stateRoot) => {
+    let response;
+
+    const url = "http://163.172.177.34:8080/beacon/validators";
+    const data = {
+      state_root: stateRoot,
+      pubkeys: [pubKey]
+    };
+    return await postData(url, data);
+  };
+
+  const postData = async (url = "", data = {}) => {
+    //@todo try / catch
+    const response = await fetch(url, {
+      method: "POST",
+      mode: "cors",
+      body: JSON.stringify(data)
+    });
+
+    let validatorInfo;
+
+    try {
+      validatorInfo = await response.json();
+      console.log(validatorInfo);
+      setActivationEligibilityEpoch(
+        validatorInfo[0].validator.activation_eligibility_epoch
+      );
+      setActivationEpoch(validatorInfo[0].validator.activation_epoch);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  fetchValidatorByPubKey(props.pubKey, props.stateRoot);
+  return (
+    <div>
+      Here's some info Eligible from Epoc {activationEligibilityEpoch}{" "}
+      Activation Epoch {activationEpoch}
+    </div>
+  );
+}
